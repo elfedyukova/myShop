ac606984ac8a0abf1c38d181c555145b45f877ee
elfedyukova <elfedyukova@yandex.ru>
2022-09-07 11:16:51 +0300

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+
+
+export default function CustomNavbar() {
+    const [variant, setVariant] = React.useState("static");
+
+    const variants = ["static", "floating", "sticky"];
+
+    return (
+
+        <Navbar isBordered variant={variant}>
+            <Navbar.Brand>
+
+                <Text b color="inherit" hideIn="xs">
+                    ACME
+                </Text>
+            </Navbar.Brand>
+            <Navbar.Content hideIn="xs">
+                <Navbar.Link href="#">Features</Navbar.Link>
+                <Navbar.Link isActive href="#">Customers</Navbar.Link>
+                <Navbar.Link href="#">Pricing</Navbar.Link>
+                <Navbar.Link href="#">Company</Navbar.Link>
+            </Navbar.Content>
+            <Navbar.Content>
+                <Navbar.Link color="inherit" href="#">
+                    Login
+                </Navbar.Link>
+                <Navbar.Item>
+                    <Button auto flat as={Link} href="#">
+                        Sign Up
+                    </Button>
+                </Navbar.Item>
+            </Navbar.Content>
+        </Navbar>
+
+
+    )
+}
